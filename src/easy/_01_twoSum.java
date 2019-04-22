7d2a2c67fcfed0566131725f7fd1a12b281007d5
yiguohan <yiguohan@gmail.com>
2019-04-22 22:43:26 +0800

    update 02

--- src/easy/_01_twoSum.java
+++ src/easy/_01_twoSum.java
@@ -1,7 +1,43 @@
 package easy;
 
+import java.util.HashMap;
+
 /**
- *
+ * 题目描述：
+ * <p>
+ * 给定一个整数数组nums和一个目标值target，请你在该数组中找出和为目标值的那两个整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素
+ * <p>
+ * 示例：
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
  */
 public class _01_twoSum {
+
+
+    private static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] test = new int[]{2, 7, 9, 11};
+        int target = 9;
+
+        int[] answer = twoSum(test, target);
+        for (int i = 0; i < answer.length; i++) {
+            System.out.println(answer[i]);
+        }
+    }
+
 }
