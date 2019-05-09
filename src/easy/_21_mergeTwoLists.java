b1611c2be9714560ed8c0a280889d9df81eede88
yiguohan <yiguohan@gmail.com>
2019-05-09 19:27:12 +0800

    add removeDuplicates

--- src/easy/_21_mergeTwoLists.java
+++ src/easy/_21_mergeTwoLists.java
@@ -8,7 +8,7 @@ package easy;
  * 输入：1->2->4, 1->3->4
  * 输出：1->1->2->3->4->4
  */
-public class _021_mergeTwoLists {
+public class _21_mergeTwoLists {
 
     public static void main(String[] args) {
 
